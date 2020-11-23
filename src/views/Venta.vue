99117ea23a9e90527ed78bfccf962fcbe2137afe
Natalia <nflores.teoria@gmail.com>
2020-11-23 19:32:23 -0300

    Subiendo array de objetos

--- src/views/Venta.vue
+++ src/views/Venta.vue
@@ -10,6 +10,9 @@ export default {
         return {
             products: [
                 {codigo: "0001", nombre: "Sekiro", stock: 100, precio: 30000, color: "red", destacado: true},
+                {codigo: "0002", nombre: "Fifa 21", stock: 100, precio: 25000, color: "blue", destacado: false},
+                {codigo: "0003", nombre: "Gears of War 4", stock: 100, precio: 15000, color: "green", destacado: true},
+                {codigo: "0004", nombre: "Mario Tennis Aces", stock: 100, precio: 35000, color: "yellow", destacado: false},
             ]
         }
     },  
